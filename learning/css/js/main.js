7be547641081758b637095dc2d7f914c17ae2878
rom18009 <rom18009@byui.edu>
2021-01-08 13:31:44 -0700

    js button

---
+++ learning/css/js/main.js
@@ -0,0 +1,10 @@
+<script>
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+  let name = prompt('Enter a new name');
+  para.textContent = 'Player 1: ' + name;
+}
+</script>
