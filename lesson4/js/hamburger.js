26df75bb2c8bbacb0c919b22a42bfb16c1beb9d6
rom18009 <rom18009@byui.edu>
2021-01-30 11:45:09 -0700

    start-navigation work

--- lesson4/js/hamburger.js
+++
@@ -1,9 +0,0 @@
-function menuToggle() {
-    document.getElementsByClassName("clearfix")[0].classList.toggle("responsive");
-
-    if (document.getElementById("hamburger").innerHTML === "Menu") {
-        document.getElementById("hamburger").innerHTML = "X";
-    } else {
-        document.getElementById("hamburger").innerHTML = "Menu";
-    }
-}
--- lesson4/js/longdates.js
+++
@@ -1,9 +0,0 @@
-const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-GB', options);
-
-document.getElementById('currentyear').textContent = new Date().getFullYear();
