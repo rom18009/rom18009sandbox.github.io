c2e0f47fb8b16219ecead9cbd7897e3b04621633
rom18009 <rom18009@byui.edu>
2021-02-22 20:52:34 -0700

    save

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,8 +1,8 @@
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
+    threshold: 0.25,
+    rootMargin: "0px 0px 200px 0px"
 };
 
 const loadImages = (image) => {
@@ -29,3 +29,33 @@ if ('IntersectionObserver' in window) {
         loadImages(img);
     });
 }
+
+// const images = document.querySelectorAll("[data-src]");
+
+// function preloadImage(img) {
+// 	const src = img.getAttribute("data-src");
+// 	if (!src) {
+// 		return;
+// 	}
+// 	img.src = src;
+// }
+
+// const imgOptions = {
+// 		threshold: 1,
+// 		rootMargin: "0px 0px 200px 0px"
+// };
+
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+// 	entries.forEach(entry => {
+// 		if(!entry.isIntersecting) {
+// 			return;
+// 		} else {
+// 		preloadImage(entry.target);
+// 		imgObserver.unobserve(entry.target);
+// 	}
+// 	})
+// }, imgOptions)
+
+// images.forEach( image => {
+// 	imgObserver.observe(image);
+// });
